97c50a77bb3cc4e35f2740a7e0b0f96b36a166a1
Kerryn Gammie <34811018+elevsev@users.noreply.github.com>
2022-07-26 07:18:01 +0100

    Create 26072022.py
    area of a square

---
+++ daily-codes/26072022.py
@@ -0,0 +1,8 @@
+def area_of_square(length):
+  '''Returns the area of a square'''
+  area = length ** 2
+  return area
+
+length = 3
+a_o_s = area_of_a_square(length=length)
+print(f"This is the area of a square with length of {length} units: {a_o_s}sq.")
