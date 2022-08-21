9b054ef02039672f950346b98e6e859428af141d
elevsev <kgammie94@gmail.com>
2022-08-21 09:35:58 +0100

    21082022 - playing with nltk

---
+++ daily-codes/21082022.py
@@ -0,0 +1,14 @@
+from cgitb import text
+import imp
+import nltk
+# nltk.download()
+
+from nltk.book import *
+
+# print(text1)
+
+# text1.concordance("whale")
+
+# text2.similar("sense")
+
+text4.dispersion_plot(['citizens', 'democracy'])
