2779a79d4fed1eac367e967cc3432b392e498f3e
elevsev <kgammie94@gmail.com>
2022-10-18 07:35:39 +0100

    18102022 - numerical transformer

---
+++ daily-codes/18102022.py
@@ -0,0 +1,9 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy='constant')
+
+print(numerical_transformer)
