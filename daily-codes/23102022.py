4bc9225c9103273f48d7c74df3804dfdc82c3015
elevsev <kgammie94@gmail.com>
2022-10-23 10:01:29 +0100

    23102022 - gross profit

---
+++ daily-codes/23102022.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mean_revenue = 170
+stdev_revenue = 20
+iterations = 1000
+
+forecasted_revenue = np.random.normal(loc=mean_revenue, scale=stdev_revenue, size=iterations)
+
+# plt.figure(figsize=(15,6))
+# plt.plot(forecasted_revenue)
+# plt.show()
+
+prop_of_rev_from_cogs = 0.6
+cog_std = 0.1
+# assume that the proportion of revenue that comes from cost of goods sold (COGS) is 60%
+cost_of_goods_sold = -1 * forecasted_revenue * np.random.normal(loc=prop_of_rev_from_cogs, scale=cog_std)
+
+# print(cost_of_goods_sold.mean())
+# print(cost_of_goods_sold.std())
+
+
+gross_profit = forecasted_revenue + cost_of_goods_sold
+# print(gross_profit)
+
+plt.figure(figsize=(15,6))
+plt.plot(gross_profit)
+plt.show()
+
+plt.figure(figsize=(15,6))
+plt.hist(gross_profit, bins='auto')
+plt.show()
